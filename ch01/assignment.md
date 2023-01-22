747769b8e8f0aa144844eb57f940804f2e7a05fc
Rey <reyhstone@gmail.com>
2023-01-22 18:03:11 -0500

    Added assignment.md and published first lab assignment

---
+++ ch01/assignment.md
@@ -0,0 +1,9 @@
+# Ch 1
+
+Chapter 1 is a review of basic Java programming such as variables, classes, loops, constructors, etc.
+
+## Assignments
+
+The assignments for this chapter are as follows:
+
+**Lab 1:** IntelliJ Idea setup and test.
---
+++ ch01/lab_1.java
@@ -0,0 +1,12 @@
+/* This program is used to test the function of a newly installed Java IDE on my computer.
+ *
+ * R Stone
+ */
+
+package ch01;
+
+public class lab_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
