b059584bf5fdfb6e2abaeab6a23225d6e07f688b
Rey <reyhstone@gmail.com>
2023-04-04 13:10:44 -0400

    New lab

---
+++ ch07/Lab9/src/SpellChecker.java
@@ -0,0 +1,120 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class SpellChecker {
+    public static void main (String[] args) throws FileNotFoundException {
+        SpellChecker spellCheck = new SpellChecker();
+
+        spellCheck.spellCheckerTest();
+    }
+
+    public Set<String> readHashWords(String filename) throws FileNotFoundException {
+        Set<String> words = new HashSet<>();
+        Scanner in = new Scanner(new File(filename));
+
+        in.useDelimiter("[^a-zA-Z]+");
+        while (in.hasNext()) {
+            words.add(in.next().toLowerCase());
+        }
+        return words;
+    }
+
+    public ArrayList<String> readArrayWords(String filename) throws FileNotFoundException {
+        ArrayList<String> words = new ArrayList<>();
+        Scanner in = new Scanner(new File(filename));
+
+        in.useDelimiter("[^a-zA-Z]+");
+        while (in.hasNext()) {
+            words.add(in.next().toLowerCase());
+        }
+        return words;
+    }
+
+    public TreeSet<String> readTreeWords(String filename) throws FileNotFoundException {
+        TreeSet<String> words = new TreeSet<>();
+
+        Scanner in = new Scanner(new File(filename));
+
+        in.useDelimiter("[^a-zA-Z]+");
+        while (in.hasNext()) {
+            words.add(in.next().toLowerCase());
+        }
+        return words;
+    }
+
+    public void spellCheckerTest() throws FileNotFoundException {
+        StopWatch stopwatch = new StopWatch();
+
+        Set<String> dictionaryHash = readHashWords("./ch07/Lab9/resources/words");
+        Set<String> dictionaryTree = readTreeWords("./ch07/Lab9/resources/words");
+        ArrayList<String> dictionaryArray = readArrayWords("./ch07/Lab9/resources/words");
+
+        Set<String> documentHash = readHashWords("./ch07/Lab9/resources/war-and-peace.txt");
+        Set<String> documentTree = readTreeWords("./ch07/Lab9/resources/war-and-peace.txt");
+        ArrayList<String> documentArray = readArrayWords("./ch07/Lab9/resources/war-and-peace.txt");
+
+
+        // HashSet test
+        int wordCount = 0;
+
+        stopwatch.start();
+
+        for (String word : documentHash) {
+            if (!dictionaryHash.contains(word)) {
+                wordCount++;
+            }
+        }
+        stopwatch.stop();
+
+        System.out.println("Words not found: " +
+                wordCount +
+                "\nWords in dictionary: " +
+                dictionaryHash.size());
+
+        System.out.println("Elapsed HashSet time: " + stopwatch.getElapsedTime() + " ms\n\n");
+        stopwatch.reset();
+
+
+        // TreeSet Test
+        wordCount = 0;
+
+        stopwatch.start();
+
+        for (String word : documentTree) {
+            if (!dictionaryTree.contains(word)) {
+                wordCount++;
+            }
+        }
+        stopwatch.stop();
+
+        System.out.println("Words not found: " +
+                wordCount +
+                "\nWords in dictionary: " +
+                dictionaryTree.size());
+
+        System.out.println("Elapsed TreeSet time: " + stopwatch.getElapsedTime() + " ms\n\n");
+        stopwatch.reset();
+
+
+        // ArrayList Test
+        wordCount = 0;
+
+        stopwatch.start();
+
+        for (String word : documentArray) {
+            if (!dictionaryArray.contains(word)) {
+                wordCount++;
+            }
+        }
+        stopwatch.stop();
+
+        System.out.println("Words not found: " +
+                wordCount +
+                "\nWords in dictionary: " +
+                dictionaryArray.size());
+
+        System.out.println("Elapsed TreeSet time: " + stopwatch.getElapsedTime() + " ms\n\n");
+        stopwatch.reset();
+    }
+}
---
+++ ch07/assignment.md
@@ -0,0 +1,7 @@
+# Ch 7
+
+Chapter 7 talks about the hash data structures as well as some common searching and sorting algorithms. There's only one assignment in this chapter, and honestly doesn't cover a lot.
+
+## Assignments
+
+**Lab 9:** This lab showchases a spell checker for 'Alice in Wonderland' and 'War and Peace' from a .txt format. This lab mainly demonstrates the time difference between HashSet, TreeSet and AraryList classes in Java. The code scans the documents for misspelled words and checks them with a dictionary that is also imported to a .txt file. Finally, a stopwatch class allows the programmer to see the time difference of each set.
