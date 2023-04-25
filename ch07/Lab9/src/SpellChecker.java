531f0a79956d761f5d14e057423900c3e3cc9a0e
Rey <94624885+exkcd@users.noreply.github.com>
2023-04-25 19:32:56 -0400

    Fixed spelling error

--- ch07/Lab9/src/SpellChecker.java
+++ ch07/Lab9/src/SpellChecker.java
@@ -1,5 +1,5 @@
 /*
- * This lab showchases a spell checker for 'Alice in Wonderland' and 'War and Peace' from a .txt format. This lab mainly demonstrates the time difference between HashSet, TreeSet and AraryList classes in Java. The code scans the documents for misspelled words and checks them with a dictionary that is also imported to a .txt file. Finally, a stopwatch class allows the programmer to see the time difference of each set.
+ * This lab showcases a spell checker for 'Alice in Wonderland' and 'War and Peace' from a .txt format. This lab mainly demonstrates the time difference between HashSet, TreeSet and AraryList classes in Java. The code scans the documents for misspelled words and checks them with a dictionary that is also imported to a .txt file. Finally, a stopwatch class allows the programmer to see the time difference of each set.
  * R Stone
  */
 
